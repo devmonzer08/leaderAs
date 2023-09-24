8434707a9c76500cfcf04906e198f49089120f01
devmonzer08 <142656890+devmonzer08@users.noreply.github.com>
2023-09-24 23:22:45 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,13 @@
+from os import getenv
+from dotenv import load_dotenv
+
+load_dotenv()
+
+API_ID = int("11371179")
+API_HASH = ("9ba892aa7ac568ab8094f609f8dca656")
+
+BOT_TOKEN = ("6638735427:AAEF1hU1ggbRKRZ_6g9ozk5dGPRvGI2bRZ0")
+OWNER_ID = int("6059195246")
+
+MONGO_DB_URI = ("mongodb+srv://albolbol:albolbolzpy@cluster0.cpd7rm5.mongodb.net/?retryWrites=true&w=majority")
+MUST_JOIN =("V_P_N_8")
